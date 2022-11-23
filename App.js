e87b8551128c80531e26a3f83bd6f636b95ad6a9
Filipe Coelho <pamela_leles_sousa@outlook.com>
2022-11-23 16:45:38 -0300

    implementing login background

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+
+import img from './assets/backgroundImage/img.jpg';
 
 export default function App(){
   return(
-    <View
-      style={{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'space-evenly',
-        backgroundColor:'black'
-      }}
-    >
+    <View>
+      <Image
+        source={img}
+        style={{
+          width:'100%',
+          height:'100%',         
+          resizeMode: "cover",
+          backgroundColor: 'transparent',
+        
+        }}
+      />
+
+      <View>
+
       {/* texto de apresentação*/ }
       <View 
         style={{
@@ -140,6 +148,7 @@ export default function App(){
           }}
         > Entrar</Text>
       </TouchableOpacity>
+      </View>
       </View>
 
 
